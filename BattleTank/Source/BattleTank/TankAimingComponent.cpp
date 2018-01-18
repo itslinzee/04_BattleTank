f3353b7a17a9f23a2f82fd27920ff4f04d47234b
DESKTOP-OKUV7LP\linds <lindsay.ortiz@live.com>
2018-01-18 15:32:53 -0600

    BT Removed Starter content

--- BattleTank/Source/BattleTank/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/TankAimingComponent.cpp
@@ -66,7 +66,7 @@ bool UTankAimingComponent::IsBarrelMoving()
 	if (!ensure(Barrel)) { return false; }
 
 	auto BarrelForward = Barrel->GetForwardVector();
-	return !BarrelForward.Equals(AimDirection, 0.01f);
+	return !BarrelForward.Equals(AimDirection, 0.1f);
 }
 
 void UTankAimingComponent::AimAt(FVector HitLocation)
